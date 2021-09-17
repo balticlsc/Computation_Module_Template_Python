8d887807c55ae944e4753db4490173774e26b375
Kamil Rybiński <kamilri@wp.pl>
2021-09-17 14:00:49 +0200

    pso #1

--- examples/particle_swarm/population_generator/module/my_token_listener.py
+++ examples/particle_swarm/population_generator/module/my_token_listener.py
@@ -1,16 +1,12 @@
+import json
+import numpy
+from numpy import ndarray, asarray
+from pyswarms import backend
+from pyswarms.backend.topology import Star
 from computation_module.api_access.job_controller import init_job_controller, TokenListener
 from computation_module.baltic_lsc.data_handler import IDataHandler
 from computation_module.baltic_lsc.job_registry import IJobRegistry
-
-# Import modules
-import numpy as np
-
-# Import sphere function as objective function
-from pyswarms.utils.functions.single_obj import sphere as f
-
-# Import backend modules
-import pyswarms.backend as P
-from pyswarms.backend.topology import Star
+from computation_module.data_model.messages import Status
 
 
 class MyTokenListener(TokenListener):
@@ -32,7 +28,31 @@ class MyTokenListener(TokenListener):
 
     def data_complete(self):
         # Place your code here:
-        pass
+        self._registry.set_status(Status.WORKING)
+        swarm_file_path = self._data.obtain_data_item('current swarm')
+        with open(swarm_file_path) as swarm_file:
+            swarm = backend.Swarm(**{key: value.tolist() if isinstance(value, ndarray) else value for key, value
+                                     in json.load(swarm_file)})
+        swarm.current_cost = asarray(list(json.loads(c)['value'] for c
+                                          in self._registry.get_pin_values('Population Fitness')))
+        swarm.pbest_pos, swarm.pbest_cost = backend.compute_pbest(swarm)
+        topology = Star()
+        if numpy.min(swarm.pbest_cost) < swarm.best_cost:
+            swarm.best_pos, swarm.best_cost = topology.compute_gbest(swarm)
+        swarm.velocity = topology.compute_velocity(swarm)
+        swarm.position = topology.compute_position(swarm)
+        data_set_handle = self._registry.get_pin_value('current data set')
+        for i in range(len(swarm.position)):
+            self._data.send_data_item('population', json.dumps(swarm.position[i].tolist()),
+                                      len(swarm.position) == len(swarm.position) - 1)
+            self._data.send_data_item('data sets', data_set_handle,
+                                      len(swarm.position) == len(swarm.position) - 1)
+        self._data.send_data_item('next data set', data_set_handle, True)
+        with open('swarm.json', 'w') as f:
+            json.dump({key: asarray(value) if isinstance(value, type([])) else value for key, value
+                       in swarm.__dict__.items()}, f)
+        self._data.send_token('next swarm', 'swarm.json', True)
+        self._data.finish_processing()
 
 
 app = init_job_controller(MyTokenListener)

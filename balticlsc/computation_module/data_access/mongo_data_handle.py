262f1efeb0cb4302f7936ecd77fa0ea001771c38
smialek <michal.smialek@ee.pw.edu.pl>
2021-10-06 13:08:37 +0200

    AccessType quick hotfix #2

--- balticlsc/computation_module/data_access/mongo_data_handle.py
+++ balticlsc/computation_module/data_access/mongo_data_handle.py
@@ -43,37 +43,36 @@ class MongoDBHandle(DataHandle):
         collection_name = handle['Collection']
         self.__prepare(handle['Database'], collection_name)
         local_path = ''
-        match self._pin_configuration.data_multiplicity:
-            case Multiplicity.SINGLE:
-                if 'ObjectId' not in handle:
-                    raise ValueError('Incorrect DataHandle.')
-                obj_id = handle['ObjectId']
-                try:
-                    logger.debug('Downloading object with id: ' + obj_id)
-                    document = self.__mongo_collection.find_one({'_id': ObjectId(obj_id)})
-                    if document is not None:
-                        local_path = _download_single_file(document, self._local_path)
-                        logger.debug('Downloading object with id: ' + obj_id + ' successful.')
-                    else:
-                        logger.debug('Can not find object with id ' + obj_id)
-                except BaseException as e:
-                    logger.debug('Downloading object with id: ' + obj_id + ' failed.')
-                    self._clear_local()
-                    raise e
-            case Multiplicity.MULTIPLE:
-                try:
-                    logger.debug('Downloading all files from ' + collection_name)
-                    local_path = self._local_path + '/' + collection_name
-                    os.mkdir(local_path)
-                    documents = self.__mongo_collection.find()
-                    for doc in documents:
-                        _download_single_file(doc, local_path)
-                    self._add_guids_to_file_names(local_path)
-                    logger.debug('Downloading all files from ' + collection_name + ' successful.')
-                except BaseException as e:
-                    logger.debug('Downloading all files from ' + collection_name + ' failed.')
-                    self._clear_local()
-                    raise e
+        if Multiplicity.SINGLE == self._pin_configuration.data_multiplicity:
+            if 'ObjectId' not in handle:
+                raise ValueError('Incorrect DataHandle.')
+            obj_id = handle['ObjectId']
+            try:
+                logger.debug('Downloading object with id: ' + obj_id)
+                document = self.__mongo_collection.find_one({'_id': ObjectId(obj_id)})
+                if document is not None:
+                    local_path = _download_single_file(document, self._local_path)
+                    logger.debug('Downloading object with id: ' + obj_id + ' successful.')
+                else:
+                    logger.debug('Can not find object with id ' + obj_id)
+            except BaseException as e:
+                logger.debug('Downloading object with id: ' + obj_id + ' failed.')
+                self._clear_local()
+                raise e
+        elif Multiplicity.MULTIPLE == self._pin_configuration.data_multiplicity:
+            try:
+                logger.debug('Downloading all files from ' + collection_name)
+                local_path = self._local_path + '/' + collection_name
+                os.mkdir(local_path)
+                documents = self.__mongo_collection.find()
+                for doc in documents:
+                    _download_single_file(doc, local_path)
+                self._add_guids_to_file_names(local_path)
+                logger.debug('Downloading all files from ' + collection_name + ' successful.')
+            except BaseException as e:
+                logger.debug('Downloading all files from ' + collection_name + ' failed.')
+                self._clear_local()
+                raise e
         return local_path
 
     def upload(self, local_path: str) -> {}:
@@ -89,32 +88,31 @@ class MongoDBHandle(DataHandle):
         handle = {}
         try:
             database_name, collection_name = self.__prepare()
-            match self._pin_configuration.data_multiplicity:
-                case Multiplicity.SINGLE:
-                    logger.debug('Uploading file from ' + local_path + 'to collection ' + collection_name)
-                    file_name = basename(local_path)
-                    with open(local_path, mode='rb') as file:
+            if Multiplicity.SINGLE == self._pin_configuration.data_multiplicity:
+                logger.debug('Uploading file from ' + local_path + 'to collection ' + collection_name)
+                file_name = basename(local_path)
+                with open(local_path, mode='rb') as file:
+                    file_content = file.read()
+                result = self.__mongo_collection.insert_one({'fileName': file_name,
+                                                             'fileContent': file_content})
+                handle = {'FileName': file_name, 'ObjectId': str(result.inserted_id), 'Database': database_name,
+                          'Collection': collection_name}
+                logger.debug('Uploading file from ' + local_path + ' to collection ' + collection_name +
+                             ' successful.')
+            elif Multiplicity.MULTIPLE == self._pin_configuration.data_multiplicity:
+                logger.debug('Uploading directory from ' + local_path + ' to collection ' + collection_name)
+                handle_list = []
+                for f in (f for f in listdir(local_path) if isfile(join(local_path, f))):
+                    file_name = basename(f)
+                    with open(f, mode='rb') as file:
                         file_content = file.read()
                     result = self.__mongo_collection.insert_one({'fileName': file_name,
                                                                  'fileContent': file_content})
-                    handle = {'FileName': file_name, 'ObjectId': str(result.inserted_id), 'Database': database_name,
-                              'Collection': collection_name}
-                    logger.debug('Uploading file from ' + local_path + ' to collection ' + collection_name +
-                                 ' successful.')
-                case Multiplicity.MULTIPLE:
-                    logger.debug('Uploading directory from ' + local_path + ' to collection ' + collection_name)
-                    handle_list = []
-                    for f in (f for f in listdir(local_path) if isfile(join(local_path, f))):
-                        file_name = basename(f)
-                        with open(f, mode='rb') as file:
-                            file_content = file.read()
-                        result = self.__mongo_collection.insert_one({'fileName': file_name,
-                                                                     'fileContent': file_content})
-                        handle_list.append({'FileName': file_name, 'ObjectId': str(result.inserted_id)})
-                    handle = {'Files': json.dumps(handle_list), 'Database': database_name,
-                              'Collection': collection_name}
-                    logger.Debug('Uploading directory from ' + local_path + 'to collection ' + collection_name +
-                                 ' successful.')
+                    handle_list.append({'FileName': file_name, 'ObjectId': str(result.inserted_id)})
+                handle = {'Files': json.dumps(handle_list), 'Database': database_name,
+                          'Collection': collection_name}
+                logger.Debug('Uploading directory from ' + local_path + 'to collection ' + collection_name +
+                             ' successful.')
             return handle
         except BaseException as e:
             logger.Debug('Error: ' + str(e) + ' \n Uploading from ' + local_path + ' failed.')
@@ -123,7 +121,6 @@ class MongoDBHandle(DataHandle):
             self._clear_local()
 
     def check_connection(self, handle: {}):
-        '''
         host = self._pin_configuration.access_credential["Host"]
         port = int(self._pin_configuration.access_credential["Port"])
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -134,7 +131,6 @@ class MongoDBHandle(DataHandle):
                 return -1
         finally:
             sock.close()
-        '''
         logger.debug(f'MongoDB connection string: {self.__connection_string}')
 
         try:

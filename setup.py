f4172391ab8e4e623fcf0f00e235001c157f4b81
K4liber <janbielecki94@gmail.com>
2021-10-06 19:16:45 +0200

    upgrade image version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="balticlsc_test",
-    version="0.1.0",
+    version="0.1.1",
     author="BalticLSC",
     author_email="Kamil.Rybinski@pw.edu.pl",
     description="Baltic LSC module old_scheme",
